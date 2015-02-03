9d4836893b28aae2d9ef215b21d739fb25ef22cd
0xc0d3r <anesh.parvatha@gmail.com>
2015-02-03 09:17:39 +0530

    Raghu vs Sayan problem is solved

---
+++ RaghuvsSayan.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+typedef long long ll;
+int comparator(const void *p,const void *q) {
+	return ( *(int *)p - *(int *)q );
+}
+int main() {
+	int t,N,i;
+	ll X,Y,sum=0;
+	scanf("%d",&t);
+	while(t--){
+		scanf("%lld%lld%d",&X,&Y,&N);
+		int cal[N],chef=0,jhool=0;
+		for(i=0;i<N;i++){
+			scanf("%d",&cal[i]);
+		}
+		qsort(cal,(size_t)N,sizeof(int),comparator);
+		for(i=0;i<N;i++){
+			sum+=cal[i];
+			if(sum<=X) chef++;
+			if(sum<=Y) jhool++;
+		}
+		sum=0;
+		puts(chef>jhool ? "Raghu Won" : chef==jhool ? "Tie" : "Sayan Won");
+	}
+}
