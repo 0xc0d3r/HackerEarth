64eebe197cd6cf545cde782843023cfed7c37a36
0xc0d3r <anesh.parvatha@gmail.com>
2015-02-14 21:49:50 +0530

    Ladder to Heaven problem is solved

--- LadderToHeaven.c
+++ LadderToHeaven.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 typedef long long int lld;
 lld ladderToHeaven(lld n){
-	lld a=1,b=2,c=a+b,sum=b,even=2;
+	lld a=1,b=2,c=a+b,sum=b;
 	while(c<=n){
 		a=b;
 		b=c;
 		c=a+b;
-		even++;
 		if(!(b&1) ) sum+=b;
 	}
 	return sum;
